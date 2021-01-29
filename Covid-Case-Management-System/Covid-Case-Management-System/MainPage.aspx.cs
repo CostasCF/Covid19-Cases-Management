32faab6d216d51aa3d1aaded04a5c732fe2690b8
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2021-01-29 21:44:40 +0200

    Created project and added model

--- Covid-Case-Management-System/Covid-Case-Management-System/MainPage.aspx.cs
+++ Covid-Case-Management-System/Covid-Case-Management-System/MainPage.aspx.cs
@@ -14,4 +14,55 @@ namespace Covid_Case_Management_System
 
         }
     }
+    public class DataEntry
+    {
+        public void enteringData(CovidCase newCovidCase)
+        {
+            Model1Container query = new Model1Container();
+            newCovidCase ncc = new newCovidCase();
+            ncc.FirstName = newCovidCase.FirstName;
+            ncc.LastName = newCovidCase.LastName;
+            ncc.PhoneNumber = newCovidCase.PhoneNumber;
+            ncc.Gender = newCovidCase.Gender;
+            ncc.Age = newCovidCase.Age;
+            ncc.Address = newCovidCase.Address;
+            ncc.Deseases = newCovidCase.Deseases;
+            ncc.Date = newCovidCase.Date;
+            query.newCovidCases.Add(ncc);
+            query.SaveChanges();
+        }
+
+        public void showingData()
+        {
+
+        }
+    }
+
+    public class CovidCase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+        public string Deseases { get; set; }
+        public string Date { get; set; }
+        public CovidCase(string firstname, string lastName, string phoneNumber, string gender, string age, string address, string deseases,string data)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Gender = gender;
+            Age = age;
+            Address = address;
+            Deseases = deseases;
+            Date = data;
+        }
+
+        public CovidCase()
+        {
+
+        }
+    }
 }
